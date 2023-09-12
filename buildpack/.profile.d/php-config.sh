7518e33311ae11dfeff66c00a46427ee05843108
harrysupertramp <enricogava98@icloud.com>
2023-09-12 21:53:56 +0200

    added comments in php-config

--- buildpack/.profile.d/php-config.sh
+++ buildpack/.profile.d/php-config.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-# Modify PHP configuration parameters
+# Set custom PHP configuration and log a message
 echo "Setting custom PHP configuration"
+
+# Modify PHP configuration parameters
 echo "default_socket_timeout = 3000" >> /app/.heroku/php/etc/php/php.ini
 echo "post_max_size = 3027M" >> /app/.heroku/php/etc/php/php.ini
 echo "upload_max_filesize = 3027M" >> /app/.heroku/php/etc/php/php.ini
+
+# Log a completion message
+echo "Custom PHP configuration applied"
